e578689e0b9d5ff0e3f93f7dbc3221e4947b0766
Kass-i <kasia.idasz@gmail.com>
2023-05-19 00:07:54 +0200

    Add Gardener first movement

--- src/GardenSimulation.java
+++ src/GardenSimulation.java
@@ -3,10 +3,15 @@ public class GardenSimulation {
         Garden garden = new Garden(10, 10);
         garden.initialize(1, 3, 2, 4);
 
-        for(int i = 0; i < 30; i++) {
+        Gardener gardener = new Gardener();
+
+        for(int i = 0; i < 250; i++) {
             System.out.println(i + 1);
             garden.update();
             garden.print();
+
+            gardener.move(garden.getSizeX(), garden.getSizeY());
+
             System.out.println();
         }
     }
--- src/Gardener.java
+++ src/Gardener.java
@@ -1,3 +1,97 @@
 public class Gardener {
+    private int posX;
+    private int posY;
+    private String horizontalDirection;
+    private String verticalDirection;
 
+    public Gardener()
+    {
+        posX = 0;
+        posY = 0;
+        horizontalDirection = "Right";
+        verticalDirection = "Forward";
+    }
+
+    public void move(int maxX, int maxY)
+    {
+        if(verticalDirection.equals("Forward"))
+        {
+            if(horizontalDirection.equals("Right")) //going forward right
+            {
+                if(posY + 1 == maxY) //out of range Y
+                {
+                    if(posX + 1 == maxX) //out of range X and Y [maxX, maxY]
+                    {
+                        verticalDirection = "Backwards";
+                        posY--;
+                    }
+                    else
+                        posX++;
+
+                    horizontalDirection = "Left";
+                }
+                else
+                    posY++;
+
+            }
+            else //going forward left
+            {
+                if(posY - 1 < 0) //out of range Y
+                {
+                    if(posX + 1 == maxX) //out of range X and Y [maxX, 0]
+                    {
+                        verticalDirection = "Backwards";
+                        posY++;
+                    }
+                    else
+                        posX++;
+
+                    horizontalDirection = "Right";
+                }
+                else
+                    posY--;
+            }
+        }
+        else
+        {
+            if(horizontalDirection.equals("Right")) //going backwards right
+            {
+                if(posY + 1 == maxY) //out of range Y
+                {
+                    if(posX - 1 < 0) //out of range X and Y [0, maxY]
+                    {
+                        verticalDirection = "Forward";
+                        posY--;
+                    }
+                    else
+                        posX--;
+
+                    horizontalDirection = "Left";
+                }
+                else
+                    posY++;
+
+            }
+            else //going backwards left
+            {
+                if(posY - 1 < 0) //out of range Y
+                {
+                    if(posX - 1 < 0) //out of range X and Y [0, 0]
+                    {
+                        verticalDirection = "Forward";
+                        posY++;
+                    }
+                    else
+                        posX--;
+
+                    horizontalDirection = "Right";
+                }
+                else
+                    posY--;
+            }
+        }
+
+        //debug
+        System.out.println(verticalDirection + ", " + horizontalDirection + " [" + posX + "," + posY + "]");
+    }
 }
